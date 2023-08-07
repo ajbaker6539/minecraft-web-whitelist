eef7fe1ea8454840aa8dc65f39ccf9582dc0f68d
Grant <justanormalowl@gmail.com>
2023-08-07 17:43:34 -0500

    Added viewing for data, accept button for new requests

--- whitelist/whitelist/view_requests/views.py
+++ whitelist/whitelist/view_requests/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse, HttpResponseNotFound
+from whitelist.requests.models import UserRequest
 # Create your views here.
 
 def view_requests(request):
-    return HttpResponseNotFound
+    if request.POST:
+        print(request.POST)
+    template = loader.get_template("view_requests.html")
+    forms = UserRequest.objects.all()
+    context = {"forms": forms}
+    return HttpResponse(template.render(context, request))
